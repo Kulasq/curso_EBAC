4ae80bdb24980a2425a3cd93767305e4d22f9beb
Kulasq <lucasdantas_11@hotmail.com>
2024-10-23 10:22:16 -0300

    editando parametros

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // URL do GitHub API para obter informações
     const apiUrl = 'https://api.github.com/users/Kulasq';
 
-    // Função para buscar e preencher os dados
     async function fetchGitHubData() {
         try {
             const response = await fetch(apiUrl);
 
-            // Verifica se a resposta é bem-sucedida
             if (!response.ok) {
                 throw new Error('Erro ao buscar dados');
             }
 
             const data = await response.json();
 
-            // Preenche os dados no HTML
             document.querySelector('.profile-avatar').src = data.avatar_url;
             document.querySelector('.profile-name').textContent = data.name || 'Nome não disponível';
             document.querySelector('.profile-username').textContent = `@${data.login}`;
@@ -27,6 +23,5 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // Chama a função para buscar os dados
     fetchGitHubData();
 });
